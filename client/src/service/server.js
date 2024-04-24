b9cb3bef991a9bc950cbb29b1fee3c36f569552b
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-04-24 22:43:31 +0530

    first commit

--- client/src/service/server.js
+++ client/src/service/server.js
@@ -1,5 +1,5 @@
 const localURL = "http://localhost:3000/api";
 
-const remoteURL = "https://spotify-project-server.vercel.app/api"
+const remoteURL = "https://mixvibe-music-video-web-app.vercel.app/api"
 
-export const server = localURL;
+export const server = remoteURL;
